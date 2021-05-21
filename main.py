2ff332b04abd4d4801ae195de75de33f917be99e
jm12138 <2286040843@qq.com>
2021-05-22 01:13:44 +0800

    updata README

--- main.py
+++ main.py
@@ -27,49 +27,83 @@ def main(args):
 
     if args.video_path:
         cap = cv2.VideoCapture(args.video_path)
-    else:
+    elif args.camera_id is not None:
         cap = cv2.VideoCapture(args.camera_id)
+    elif args.img_dir:
+        imgs = [os.path.join(args.img_dir, img) for img in os.listdir(args.img_dir)]
+        imgs.sort()
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     if args.save_dir:
         if not os.path.exists(args.save_dir):
             os.mkdir(args.save_dir)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        print(fps, w, h)
         save_video_path = os.path.join(args.save_dir, 'output.avi')
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        writer = cv2.VideoWriter(
-            save_video_path, fourcc, fps, (int(w), int(h)))
+        if (args.video_path) or (args.camera_id is not None):
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            w = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            writer = cv2.VideoWriter(
+                save_video_path, fourcc, fps, (int(w), int(h)))
+        else:
+            im = cv2.imread(imgs[0])
+            h, w, _ = im.shape
+            writer = cv2.VideoWriter(
+                save_video_path, fourcc, 30, (int(w), int(h)))
+
 
     frame_id = 0
     track_outputs = []
-    while True:
-        success, frame = cap.read()
-        frame_id += 1
-        if not success:
-            break
-        outputs = deepsort.update(frame)
-        if outputs is not None:
-            for output in outputs:
-                cv2.rectangle(
-                    frame, (output[0], output[1]), (output[2], output[3]), (0, 0, 255), 2)
-                cv2.putText(frame, str(
-                    output[-1]), (output[0], output[1]), font, 1.2, (255, 255, 255), 2)
-                x1, y1, x2, y2, track_id = output
-                output = ' '.join(str(x) for x in [frame_id, track_id, x1, y1, x2-x1, y2-y1, 1, -1, -1, -1])
-                print(output)
-                track_outputs.append(output)
-        if args.save_dir:
-            writer.write(frame)
-        if args.display:
-            cv2.imshow('test', frame)
-            k = cv2.waitKey(1)
-            if k == 27:
-                cap.release()
+    if (args.video_path) or (args.camera_id is not None):
+        while True:
+            success, frame = cap.read()
+            frame_id += 1
+            if not success:
                 break
+            outputs = deepsort.update(frame)
+            if outputs is not None:
+                for output in outputs:
+                    cv2.rectangle(
+                        frame, (output[0], output[1]), (output[2], output[3]), (0, 0, 255), 2)
+                    cv2.putText(frame, str(
+                        output[-1]), (output[0], output[1]), font, 1.2, (255, 255, 255), 2)
+                    x1, y1, x2, y2, track_id = output
+                    output = ' '.join(str(x) for x in [frame_id, track_id, x1, y1, x2-x1, y2-y1, 1, -1, -1, -1])
+                    print(output)
+                    track_outputs.append(output)
+            if args.save_dir:
+                writer.write(frame)
+            if args.display:
+                cv2.imshow('preview', frame)
+                k = cv2.waitKey(1)
+                if k == 27:
+                    break
+        
+        cap.release()
+
+    elif args.img_dir:
+        for img in imgs:
+            frame = cv2.imread(img)
+            frame_id += 1
+            outputs = deepsort.update(frame)
+            if outputs is not None:
+                for output in outputs:
+                    cv2.rectangle(
+                        frame, (output[0], output[1]), (output[2], output[3]), (0, 0, 255), 2)
+                    cv2.putText(frame, str(
+                        output[-1]), (output[0], output[1]), font, 1.2, (255, 255, 255), 2)
+                    x1, y1, x2, y2, track_id = output
+                    output = ' '.join(str(x) for x in [frame_id, track_id, x1, y1, x2-x1, y2-y1, 1, -1, -1, -1])
+                    print(output)
+                    track_outputs.append(output)
+            if args.save_dir:
+                writer.write(frame)
+            if args.display:
+                cv2.imshow('preview', frame)
+                k = cv2.waitKey(1)
+                if k == 27:
+                    break
 
     if args.save_dir:
         writer.release()
@@ -77,9 +111,7 @@ def main(args):
         with open(os.path.join(args.save_dir, 'result.txt'), 'w') as f:
             for line in track_outputs:
                 f.write(line+'\n')
-    
-    cap.release()
-
+            
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         usage='''you can set the video_path or camera_id to start the program, 
@@ -133,8 +165,10 @@ if __name__ == '__main__':
 
     # I/O
     parser.add_argument("--video_path", type=str, default=None,
-                        help="the input video path or the camera id.")
-    parser.add_argument("--camera_id", type=int, default=0,
+                        help="the input video path.")
+    parser.add_argument("--img_dir", type=str, default=None,
+                        help="the input img dir.")
+    parser.add_argument("--camera_id", type=int, default=None,
                         help="do you want to use the camera and set the camera id.")
     parser.add_argument("--display", action="store_true",
                         help="do you want to display the results.")
