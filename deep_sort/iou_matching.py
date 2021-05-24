f6bf6e59913b5203c08d3e9a75bf01e5a40ff330
jm12138 <2286040843@qq.com>
2021-05-24 10:38:40 +0800

    update code format

--- deep_sort/iou_matching.py
+++ deep_sort/iou_matching.py
@@ -76,6 +76,8 @@ def iou_cost(tracks, detections, track_indices=None,
             continue
 
         bbox = tracks[track_idx].to_tlwh()
-        candidates = np.asarray([detections[i].tlwh for i in detection_indices])
+        candidates = np.asarray(
+            [detections[i].tlwh for i in detection_indices]
+        )
         cost_matrix[row, :] = 1. - iou(bbox, candidates)
     return cost_matrix
